5d8e15d1bf9a8f220e2ca59e8a11958ff693963c
Evan Lezar <mail@evanlezar.com>
2012-06-23 18:37:04 +0200

    Added some comments and cleaned the structure of the code.

--- src/dense_cpu.c
+++ src/dense_cpu.c
@@ -1,15 +1,15 @@
 /*
  * Evan Lezar
- * 18 November 2010
- *
- * Implementation of dense generalized eigenvalue solver using arpack
+ * An implementation of a dense eigensolver that uses the ARPACK Fortran backend.
  */
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <omp.h>
 #include <acml.h>
 
+// Include the interfaces to the Fortran routines.
 #include "interface.h"
 
 #define ARPACK_CALL 0
@@ -30,6 +30,14 @@ void print_data ( const char* desc, data_struct DATA )
     printf ( "%s: A = %p LDA = %d\n", desc, DATA.A, DATA.LDA );
 }
 
+//!
+//! Calculate the matrix-vector product  y <- Ax for the eigensystem defined by DATA. This
+//! routine is called from the Fortran backend whenever ARPACK requires a matrix-vector
+//! product to be calculated.
+//! \param[in] N The dimension of the eigensystem.
+//! \param[in,out] DATA The structure representing the eigenproblem.
+//! \param[in] x The vector that must be multiplied by A.
+//! \param[out] y The vector that must store the result.
 void sgemv_wrapper ( int N, data_struct* DATA, float* x, float* y )
 {
     checkpoint t0 = tic();
@@ -45,6 +53,8 @@ void sgemv_wrapper ( int N, data_struct* DATA, float* x, float* y )
     DATA->sgemv_calls += 1;
 }
 
+//!
+//! Initialise the struct representing the eigensystem.
 void init_data ( data_struct* DATA, float* A, int LDMAT )
 {
     DATA->A = A;
@@ -53,7 +63,9 @@ void init_data ( data_struct* DATA, float* A, int LDMAT )
     DATA->sgemv_time = 0.0;
 }
 
-
+//!
+//! Solve the actual eigensystem. This takes the eigensystem defined in the structure DATA and allocates
+//! the required temporary workspaces before calling the Fortran backend.
 void calculate_eigen_values ( int N, void* DATA, int NEV, float* eigenvalues, float* eigenvectors, char* which )
 {
     int use_N_ev = 2*NEV;
@@ -83,7 +95,16 @@ void calculate_eigen_values ( int N, void* DATA, int NEV, float* eigenvalues, fl
 }
 
 
-// solve the standard eigensystem Ax = lx
+//!
+//! Solve the standard eigensystem Ax = lx.eigen
+//! \param[in] N The number of columns in the matrix A
+//! \param[in] A The matrix representing the eigensystem to be solved.
+//! \param[in] NEV The number of eigenvalues to calculate.
+//! \param[out] eigenvalues A vector of the NEV eigenvalues. Note that the eigenvectors are complex.
+//! \param[out] eigenvectors An NxNEV matrix with the eigenvectors as columns.
+//! \param[out] timing_data_10 A 10-vector representing the timing data for various phases of the process.
+//! \param[out] int_data_10 A 10-vector containing some run information.
+//! \return A non-zero error code if an error occured.
 int dense_seev ( int N, float* A, int LDMAT, int NEV, float* eigenvalues, float* eigenvectors, double* timing_data_10, int* int_data_10 )
 {
     checkpoint t0;
@@ -102,5 +123,3 @@ int dense_seev ( int N, float* A, int LDMAT, int NEV, float* eigenvalues, float*
     int_data_10[SGEMV] = DATA.sgemv_calls;
     return result;
 }
-
-
--- test/python_driver.py
+++ test/python_driver.py
@@ -11,44 +11,51 @@ def as_pointer(A, ctype_type = None):
     return A.ctypes.data_as(ct.POINTER(ctype_type))
 
 def main():
+	# Load the shared library.
 	lib = ct.CDLL('../lib/libsingle_dense_cpu.so')
+
+	# Select the function to call.
+	# This has the prototype
+	# dense_seev(int, float*, int, int, float*, float*, double*, int*)
 	func = lib['dense_seev']
 	func.argtypes = [ct.c_int, ct.POINTER(ct.c_float), 
 					 ct.c_int, ct.c_int, 
 					 ct.POINTER(ct.c_float), ct.POINTER(ct.c_float),
 					 ct.POINTER(ct.c_double), ct.POINTER(ct.c_int)]
 
+	# Allocate storage for timing and info.
 	info_fp = np.zeros(10, dtype=ct.c_double)
 	info_int = np.zeros(10, dtype=ct.c_int)
 
+	# Setup the eigenvalue problem.
 	N = 100
-	LDA = 100
-
-	NEV = 10
-
+	LDA = N
 	A = np.diag(np.arange(1,N+1),k=0).astype(ct.c_float)
 
+	# The number of eigenvalues and storage for the eigenvalues and eigenvectors.
+	NEV = 10
 	eigenvalues = np.zeros(NEV, dtype=np.complex64)
 	eigenvectors = np.zeros((N,NEV), dtype=ct.c_float)
 
-
+	# call the loaded C function.
 	func(N, as_pointer(A, ct.c_float), 
 		LDA, NEV, 
 		as_pointer(eigenvalues, ct.c_float), as_pointer(eigenvectors, ct.c_float), 
 		as_pointer(info_fp, ct.c_double), as_pointer(info_int, ct.c_int))
 
-
-	print eigenvalues
-
+	# Output the results.
 	max_eig = N
-
+	print 'Results:'
 	print '%5s:' % 'index', '%15s' % ('calculated'), '%15s' % ('reference'), '%15s' % 'relative error'
 	for i in range(len(eigenvalues)):
 		ref = max_eig-i
 		err = np.abs(eigenvalues[i] - ref)/np.abs(ref)
 		print '%5d:' % (i+1), '%15s' % (eigenvalues[i]), '%15s' % (ref), '%15.3e' % err
 
+	print 'Timing:'
+	print info_fp
+	print info_int
 
-
+	
 if __name__ == '__main__':
 	main()
